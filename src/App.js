05bd02da86870d7186e5bf68ac961cb8489de9de
amitka <amitka@checkpoint.com>
2019-09-18 16:50:51 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>Contact Sheet</span>
+    </div>
+  );
+}
+
+export default App;
