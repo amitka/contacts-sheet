496497748421253dc3658a7cbaa99d521b13acf1
amitka <amitka@checkpoint.com>
2019-09-19 12:33:12 +0300

    layout ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
 import React from "react";
+import {
+  Link,
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import "./App.css";
 
-//let contacts = require("./assets/contacts.json");
+const contacts = require("./contacts.json");
+
+const ContactPage = ({ match, location }) => {
+  const {
+    params: { contactId }
+  } = match;
+
+  return (
+    <article className="contact-container">
+      <span>{contactId}</span>
+    </article>
+  );
+};
+
+const AllContacts = () => {
+  return (
+    <section className="all-contacts-page">
+      <header>
+        <div className="logo-container">
+          <div className="logo-circ">
+            <span className="title">גן נופר</span>
+            <span className="sub-title">2019/20</span>
+          </div>
+        </div>
+        <div className="search-container">
+          <input type="text" placeholder="Search..." />
+        </div>
+      </header>
+      <div className="contacts-container">
+        <div className="contacts-scroll">
+          {contacts.map((contact, index) => (
+            <div className="contact-item" key={index}>
+              <Link to={`/contacts/${index}`} className="link">
+                <span className="contact-pic"></span>
+                <span className="contact-name">{`${contact.first} ${contact.last}`}</span>
+              </Link>
+              <a
+                href={`tel:${contact.mobile1}`}
+                className="contact-phone"
+              >{`${contact.mobile1}`}</a>
+            </div>
+          ))}
+        </div>
+      </div>
+      <footer></footer>
+    </section>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <span>{JSON.stringify(contacts, 0, null)}</span>
-    </div>
+    <main className="contacts-app">
+      <Router>
+        <Switch>
+          <Route exact path="/contacts" component={AllContacts} />
+          <Route exact path="/contacts/:contactId" component={ContactPage} />
+          <Redirect from="/" to="/contacts" />
+        </Switch>
+      </Router>
+    </main>
   );
 }
 
