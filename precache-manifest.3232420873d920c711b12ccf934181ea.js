c7cc365295455fd6a4b37f8d279cf10567279948
amitka <amitka@checkpoint.com>
2019-09-19 13:01:43 +0300

    Updates

---
+++ precache-manifest.3232420873d920c711b12ccf934181ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cf0855d26054b8a746539229b52d2e",
+    "url": "/contacts-sheet/index.html"
+  },
+  {
+    "revision": "fee155fcf20128b61ef9",
+    "url": "/contacts-sheet/static/css/main.cf4d61e2.chunk.css"
+  },
+  {
+    "revision": "5e980ee84c3e1eed27ca",
+    "url": "/contacts-sheet/static/js/2.2da29814.chunk.js"
+  },
+  {
+    "revision": "fee155fcf20128b61ef9",
+    "url": "/contacts-sheet/static/js/main.d4670149.chunk.js"
+  },
+  {
+    "revision": "5c3da9a978bedc9cc947",
+    "url": "/contacts-sheet/static/js/runtime~main.5d8c9fbc.js"
+  }
+]);
