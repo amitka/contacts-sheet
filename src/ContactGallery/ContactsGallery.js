0e6bfe79597a583b44e46bbeaffea079888d5062
Amitka <amitka111@gmail.com>
2019-09-23 00:35:45 +0300

    no results

--- src/ContactGallery/ContactsGallery.js
+++ src/ContactGallery/ContactsGallery.js
@@ -7,6 +7,14 @@ const WHATS_APP_URL = "https://api.whatsapp.com/send?phone=972";
 
 export const ContactsGallery = () => {
   const [query, setQuery] = useState("");
+  const [itemsFound, setItemsFound] = useState([]);
+
+  useEffect(() => {
+    const found = CONTACTS_DATA.filter(contact => {
+      return contact.first.includes(query) || contact.last.includes(query);
+    });
+    setItemsFound(found);
+  }, [query]);
 
   return (
     <section className="all-contacts-page">
@@ -26,47 +34,53 @@ export const ContactsGallery = () => {
       </header>
       <div className="contacts-container">
         <div className="contacts-scroll">
-          {CONTACTS_DATA.filter(contact => {
-            return (
-              contact.first.includes(query) || contact.last.includes(query)
-            );
-          }).map((contact, index) => (
-            <div className="contact-item" key={index}>
-              <Link to={`/contacts/${index}`} className="link">
-                <div className="contact-pic">
-                  <img
-                    src={
-                      contact.pic
-                        ? require(`../assets/pics/${contact.pic}.jpg`)
-                        : require("../assets/pics/default.jpg")
-                    }
-                    alt="contact-pic"
-                  />
-                </div>
-                <span className="contact-name">{`${contact.first} ${contact.last}`}</span>
-              </Link>
-              {contact.mobile1 && (
-                <div className="contact-actions">
-                  <a href={`tel:${contact.mobile1}`} className="contact-phone">
-                    {Icons.phone}
-                  </a>
-                  <a
-                    href={`${WHATS_APP_URL + contact.mobile1}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="contact-whats-app"
-                  >
-                    {Icons.whats2}
-                  </a>
-                </div>
-              )}
-            </div>
-          ))}
+          {itemsFound.length > 0 ? (
+            itemsFound.map((item, index) => (
+              <GalleryItem contact={item} key={index} idx={index} />
+            ))
+          ) : (
+            <div className="no-results"> ... לא מצאתי</div>
+          )}
         </div>
         <footer>
           <span>made with {Icons.heart}</span>
         </footer>
       </div>
     </section>
+  );
+};
+
+const GalleryItem = ({ contact, idx }) => {
+  return (
+    <div className="contact-item">
+      <Link to={`/contacts/${idx}`} className="link">
+        <div className="contact-pic">
+          <img
+            src={
+              contact.pic
+                ? require(`../assets/pics/${contact.pic}.jpg`)
+                : require("../assets/pics/default.jpg")
+            }
+            alt="contact-pic"
+          />
+        </div>
+        <span className="contact-name">{`${contact.first} ${contact.last}`}</span>
+      </Link>
+      {contact.mobile1 && (
+        <div className="contact-actions">
+          <a href={`tel:${contact.mobile1}`} className="contact-phone">
+            {Icons.phone}
+          </a>
+          <a
+            href={`${WHATS_APP_URL + contact.mobile1}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="contact-whats-app"
+          >
+            {Icons.whats2}
+          </a>
+        </div>
+      )}
+    </div>
   );
 };
--- src/ContactPage/ContactPage.js
+++ src/ContactPage/ContactPage.js
@@ -1,50 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const contacts = require("../assets/contacts.json");
+const CONTACTS_DATA = require("../assets/contacts.json");
 
 export const ContactPage = ({ match, location }) => {
   const {
     params: { contactId }
   } = match;
+
   const [contact, setContact] = useState({});
 
   useEffect(() => {
-    //console.log(contacts[contactId]);
-    setContact(contacts[contactId]);
+    debugger;
+    const data = CONTACTS_DATA.find(
+      item => item.index === match.params.contactId
+    );
+    console.log(data);
+    setContact(CONTACTS_DATA[match.params.contactId]);
   }, []);
 
   return (
     <article className="contact-page-container">
-      <div className="contact-details">
-        <div className="page-pic">
-          <img
-            src={
-              contact.pic
-                ? require(`../assets/pics/${contact.pic}.jpg`)
-                : require("../assets/pics/default.jpg")
-            }
-            alt="contact-pic"
-          />
+      {contact && (
+        <div className="contact-details">
+          <div className="page-pic">
+            <img
+              src={
+                contact.pic
+                  ? require(`../assets/pics/${contact.pic}.jpg`)
+                  : require("../assets/pics/default.jpg")
+              }
+              alt="contact-pic"
+            />
+          </div>
+          <span className="page-name">{`${contact.first} ${contact.last}`}</span>
+          <div className="details-row">
+            <span>{contact.parent1}</span>
+            <a href={`tel:${contact.mobile1}`} className="page-phone">
+              {contact.mobile1}
+            </a>
+          </div>
+          <div className="details-row">
+            <span>{contact.parent2}</span>
+            <a href={`tel:${contact.mobile2}`} className="page-phone">
+              {contact.mobile2}
+            </a>
+          </div>
+          <br />
+          <br />
+          <span>{contact.address}</span>
+          <a href={`mailto:${contact.mail}`}>{contact.mail}</a>
         </div>
-        <span className="page-name">{`${contact.first} ${contact.last}`}</span>
-        <div className="details-row">
-          <span>{contact.parent1}</span>
-          <a href={`tel:${contact.mobile1}`} className="page-phone">
-            {contact.mobile1}
-          </a>
-        </div>
-        <div className="details-row">
-          <span>{contact.parent2}</span>
-          <a href={`tel:${contact.mobile2}`} className="page-phone">
-            {contact.mobile2}
-          </a>
-        </div>
-        <br />
-        <br />
-        <span>{contact.address}</span>
-        <a href={`mailto:${contact.mail}`}>{contact.mail}</a>
-      </div>
+      )}
       <div className="back-button-container">
         <Link to="/">חזרה לדף הקשר</Link>
       </div>
